e0c5d247f6b219a7c0623a9caa27152fb9a799f9
hearthsfx <hearthsfx@gmail.com>
2022-06-28 14:08:26 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01f87311628507f605355d424132045b.js"
+  "/precache-manifest.a2ce7e3ed0f6ba43b16640f5000d1c0b.js"
 );
 
 workbox.skipWaiting();
